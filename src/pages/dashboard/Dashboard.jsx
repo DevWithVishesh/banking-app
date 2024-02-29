c914d9c58217b93e507a3b527bad44a2d3e08fc7
DevWithVishesh <visheshkuhar143@gmail.com>
2024-02-29 15:44:16 +0530

    remove unUsed files and extra code

---
+++ src/pages/dashboard/Dashboard.jsx
@@ -0,0 +1,11 @@
+import Header from "../../components/header/Header";
+
+function Dashboard() {
+  return (
+    <div className="dashboard">
+      <Header />
+    </div>
+  );
+}
+
+export default Dashboard;
