9b2d6b932ed3383e576fd68c6d87250a4da46eac
DevWithKamal <bajajkamal076@gmail.com>
2024-02-29 16:00:00 +0530

    This is test

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Dashboard from './pages/dashboard/Dashboard';
 
+function testGit() {
+  console.log("This is Git Test")
+}
 function App() {
   return (
     <BrowserRouter>
